fc12bdedf61815e72ece5dcb69c51f82e6883227
hwempire <seozo001@naver.com>
2025-03-11 10:36:53 +0900

    success siuuuuuuuuuuuuuuuuuuu!!!!

--- Math.fs
+++ Math.fs
@@ -6,4 +6,13 @@ open System
 /// 32-bit integer, and returns the number of steps it takes to reach 1 in the
 /// Collatz sequence.
 let collatz n =
-  0
+  let iseven n = 
+    if n % 2UL = 0UL then true
+    else false
+  let rec result count x =
+    if x=1UL then count
+    else 
+    if iseven x then result (count+1) (x/2UL)
+    else result (count+1) (3UL*x+1UL)
+  printfn "Collatz conjecture for %d" n
+  result 0 (uint64 n)
--- Program.fs
+++ Program.fs
@@ -2,10 +2,12 @@
 
 open CS220.Quiz1.Math
 
+
+
 [<EntryPoint>]
 let main _args =
   printfn "You can test your program by directly modifying Program.fs"
   printfn "Or, you can change the unit tests in the `tests` directory."
   printfn "---"
-  collatz 4 |> printfn "# of steps to reach one = %d"
-  0
+  collatz 3000 |> printfn "# of steps to reach one = %d"
+  11
